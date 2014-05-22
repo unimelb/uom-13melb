c2c4e60cf9b19846e4f6a2d615f669a460d84a77
Martin Kwok <me@synook.net>
2014-05-22 14:36:55 +1000

    Area part of API almost complete, except for parts that interact with collections.

--- api.js
+++ api.js
@@ -1,5 +1,8 @@
+"use strict";
+
 var neo4j = require("neo4j");
 var q = require("q");
+var util = require("util");
 
 //var server = new neo4j.GraphDatabase("http://localhost:7474");
 
@@ -12,6 +15,7 @@ var Directory = function (server) {
 }
 
 Directory.prototype.root_area = function () {
+	var dir = this;
 	var query = [
 		"MATCH (root:Area)",
 		"WHERE NOT ()-[:PARENT_OF]->(root)",
@@ -21,11 +25,11 @@ Directory.prototype.root_area = function () {
 	this.server.query(query, {}, function (err, results) {
 		if (err) {
 			deferred.reject(err);
-			return;
+			throw err;
 		}
 		var node = results[0]["root"]; // should only be one result
 		var data = node.data;
-		var area = new Area(this, node.id, data);
+		var area = new Area(dir, node.id, data);
 		deferred.resolve(area);
 		return;
 	});
@@ -33,14 +37,15 @@ Directory.prototype.root_area = function () {
 }
 
 Directory.prototype.area = function (area_id) {
+	var dir = this;
 	var deferred = q.defer();
 	this.server.getNodeById(area_id, function (err, node) {
 		if (err) {
 			deferred.reject(err);
-			return;
+			throw err;
 		}
 		var data = node.data;
-		var area = new Area(this, node.id, data);
+		var area = new Area(dir, node.id, data);
 		deferred.resolve(area);
 		return;
 	});
@@ -60,41 +65,120 @@ var Area = function (dir, area_id, area_info) {
 	if (area_info.notes !== undefined) this.notes = area_info.notes;
 }
 
-Area.prototype.descend_along_path = function () {
+Area.prototype.descend_along_path = function (path) {
+	var area = this;
+	var deferred = q.defer();
 
+	var query = ["START n=node({root_area_id}) MATCH (n)"];
+	var params = {"root_area_id" : this.get_area_id()};
+	path.forEach(function (area_name, depth) {
+		query.push(util.format(
+			"-[:PARENT_OF]->(x%d:Area { name: \"%s\"})",
+			depth, area_name
+		));
+	});
+	var result_node_name = util.format("x%d", path.length - 1);
+	query.push(util.format("RETURN %s", result_node_name));
+	var query_string = query.join(" ");
+
+	area.directory.server.query(query_string, params,
+		function (err, target_node) {
+			if (err) {
+				deferred.reject(err);
+				throw err;
+			}
+			var target = target_node[0][result_node_name];
+			var target_area = new Area(
+				area.directory, target.id, target.data
+			);
+			deferred.resolve(target_area);
+		}
+	);
+	return deferred.promise;
 }
 
-Area.prototype.children = function (area) {
+Area.prototype.parent = function () {
+	var area = this;
 	var query = [
 		"START n=node({area_id})",
-		"MATCH (n)-[:PARENT_OF]->(m:Area)",
-		"RETURN m"
+		"MATCH (parent:Area)-[:PARENT_OF]->(n)",
+		"RETURN parent"
 	].join(" ");
-	var params = {"area_id" : area.area_id};
+	var params = {"area_id" : this.area_id};
+	var deferred = q.defer();
+	this.directory.server.query(query, params, function (err, parents) {
+		if (err) {
+			deferred.reject(err);
+			throw err;
+		}
+		var parent = parents[0]["parent"];
+		var parent_area = new Area(area.directory, parent.id, parent.data);
+		deferred.resolve(parent_area);
+	});
+	return deferred.promise;
+}
+
+Area.prototype.children = function () {
+	var area = this;
+	var query = [
+		"START n=node({area_id})",
+		"MATCH (n)-[:PARENT_OF]->(child:Area)",
+		"RETURN child"
+	].join(" ");
+	var params = {"area_id" : this.area_id};
 	var deferred = q.defer();
 	this.directory.server.query(query, params, function (err, child_nodes) {
 		if (err) {
 			deferred.reject(err);
-			return;
+			throw err;
 		}
 		var children = child_nodes.map(function (child_node) {
-			return new Area(this.directory, child_node.id, child_node.data);
+			var child_area = child_node["child"];
+			return new Area(area.directory, child_area.id, child_area.data);
 		});
 		deferred.resolve(children);
 	});
 	return deferred.promise;
 }
 
-Area.prototype.descendents = function () {
-
-}
-
-Area.prototype.filter = function () {
-	
+Area.prototype.search = function (search_str) {
+	var area = this;
+	var query = [
+		"START n=node({area_id})",
+		"MATCH (n)-[:PARENT_OF*]->(target:Area)",
+		util.format("WHERE target.name =~ \"(?i).*%s.*\"", search_str),
+		"RETURN target"
+	].join(" ");
+	var params = {"area_id" : this.area_id};
+	var deferred = q.defer();
+	this.directory.server.query(query, params, function (err, results) {
+		if (err) {
+			deferred.reject(err);
+			throw err;
+		}
+		var result_areas = results.map(function (result) {
+			var result_area = result["target"];
+			return new Area(area.directory, result_area.id, result_area.data);
+		});
+		deferred.resolve(result_areas);
+	});
+	return deferred.promise;
 }
 
 Area.prototype.collections = function () {
 
+}
+
+Area.prototype.get_area_id = function () {
+	return this.area_id;
+}
+
+Area.prototype.get_name = function () {
+	return this.name;
+}
+
+Area.prototype.get_notes = function () {
+	return this.notes;
 }
 
 exports.Area = Area;
@@ -118,3 +202,10 @@ var Contact = function () {
 }
 
 exports.Contact = Contact;
+
+/*var query_results_to_areas = function (directory, results, node_name) {
+	return results.map(function (result) {
+		var result_area = result[node_name];
+		return new Area(directory, result_area.id, result_area.data);
+	});
+}*/
--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var chai = require("chai")
 	, chaiAsPromised = require("chai-as-promised")
 	, uom_13melb = require("../api")
@@ -10,26 +12,27 @@ chai.use(chaiAsPromised);
 var expect = chai.expect;
 
 var server = new neo4j.GraphDatabase("http://localhost:7474");
+var directory;
+
+beforeEach(function () {
+	directory = new api.Directory(server);
+})
 
 // Encapsulates the entire directory system
 describe("Directory", function () {
 
 	// creates a new directory with the given Neo4J instance
-	describe("Directory", function() {
-		it("should be able to query the neo4j database", function (done) {
-			var directory = new api.Directory(server);
-			var query = "MATCH (x) RETURN x LIMIT 1";
-			directory.server.query(query, {}, function (err, results) {
-				expect(err).to.be.null;
-				done();
-			});
+	it("should be able to query the neo4j database", function (done) {
+		var query = "MATCH (x) RETURN x LIMIT 1";
+		directory.server.query(query, {}, function (err, results) {
+			expect(err).to.be.null;
+			done();
 		});
 	});
 
 	// returns the root area for the directory (i.e., the one with no parents)
 	describe("#root_area()", function () {
 		it("should return the root area", function (done) {
-			var directory = new api.Directory(server);
 			expect(directory.root_area())
 				.to.eventually.have.property("name", "13MELB").notify(done);
 		});
@@ -38,19 +41,22 @@ describe("Directory", function () {
 	// returns the area with given area ID
 	describe("#area()", function () {
 		it("should return the area with the ID", function (done) {
-			var directory = new api.Directory(server);
+			var some_child;
 			directory.root_area()
 				.then(function (area) {
 					return area.children();
 				})
 				.then(function (children) {
-					return directory.area(children[0]);
+					some_child = children[0];
+					return directory.area(some_child.get_area_id());
 				})
-				.then(function (some_child) {
-					var new_by_id = directory.area(some_child.area_id);
-					expect(new_by_id.area_id).to.equal(some_child.area_id);
+				.then(function (new_by_id) {
+					expect(new_by_id.get_area_id()).to.equal(
+						some_child.get_area_id()
+					);
 					done();
 				})
+			;
 			
 		});
 
@@ -66,89 +72,174 @@ describe("Directory", function () {
 describe("Area", function () {
 
 	// creates an area with the information
-	describe("Area", function () {
-		it("should create a new area with the given information");
+	it("should contain the correct area information", function () {
+
+		var path = [
+			"BSB Student Services",
+			"Student Support Contact List",
+			"Student Support Services",
+			"General enquiries"
+		];
+
+		directory.root_area(function (area) {
+			return area.descend_along_path(path);
+		}).then(function (area) {
+			expect(area.get_name()).to.equal("General enqiries");
+			expect(area.get_notes()).to.equal("");
+			done();
+		});
 	});
 
 	// returns the area at the end of the path from the given area
 	// probably only for internal use (i.e. when testing!)
 	// -> Area
 	describe("#descend_along_path()", function () {
-		it("should return the area at the given path", function () {
-			[
-				{
-					actual : [
-						"BSB Student Support Services",
-						"Student Support Contact List",
-						"Student Support Services"
-					],
-					expected : [
-						"BSB Student Support Services",
-						"Student Support Contact List",
-						"Student Support Services"
-					]
-				}
-			].forEach(function (example) {
-				var area = new Area();
-				var actual = area.descend_along_path(example.expected);
-				var expected = area;
-				example.expected.every(function (desired_child) {
-					var children = expected.children();
-					var matched_child = children.some(function (child) {
-						expected = child;
-						return expected.name == desired_child.name;
-					});
-					return matched_child;
-				});
-				expect(actual.area_id).to.equal(expected.area_id);
-			});
+		it("should return the area at the given path", function (done) {
+			var path = [
+				"BSB Student Services",
+				"Student Support Contact List",
+				"Student Support Services"
+			];
+
+			directory.root_area()
+				.then(function (area) {
+					return area.descend_along_path(path);
+				})
+				.then(function (area) {
+					expect(area.get_name()).to.equal(path[path.length - 1]);
+					done();
+				})
+			;
+		});
+	});
+
+	describe("#parent()", function () {
+		it("should return the parent of the area", function (done) {
+			var path = [
+				"BSB Student Services",
+				"Student Support Contact List",
+				"Student Support Services"
+			];
+
+			directory.root_area()
+				.then(function (area) {
+					return area.descend_along_path(path);
+				})
+				.then(function (area) {
+					return area.parent();
+				})
+				.then(function (parent) {
+					expect(parent.get_name()).to.equal(path[path.length - 2]);
+					done();
+				}, function (err) { console.log(err); })
+			;
 		});
 	});
 
 	// returns the children of the area
 	// -> [Area]
 	describe("#children()", function () {
-		it("should return at least one actual child of the area", function() {
-			[
-				{
-					path : [
-						"BSB Student Support Services",
-						"Student Support Contact List",
-						"Student Support Services"
-					],
-					child : "Housing"
-				}
-			].forEach(function (example) {
-				var area = new Area();
-				var children = area.descend_along_path(example.path).children();
-				expect(children)
-			});
-		});
+		it("should return at least one actual child of the area",
+			function(done) {
+				var path = [
+					"BSB Student Services",
+					"Student Support Contact List",
+					"Student Support Services"
+				];
 
-		it("should return the same number of children as the area's child count");
-		
+				directory.root_area()
+					.then(function (area) {
+						return area.descend_along_path(path);
+					})
+					.then(function (area) {
+						return area.children();
+					})
+					.then(function (children) {
+						expect(children.map(function (child) {
+							return child.get_name();
+						})).to.include("Housing");
+						done();
+					})
+				;
+			}
+		);
 	});
+
+	// USEFULNESS QUESTIONABLE - overengineering?
+	// can just use children() repeatedly, at least for the moment
 
 	// users often just want to see everything in a certain area
 	// maybe even auto-expand based on descendent count?
-	// -> {Area : {Area : ...}}
-	describe("#descendents()", function () {
-		it("should return at least one non-child descendent in the tree");
-		it("should return descendents equalling the descendent count");
-	});
+	// -> [{Area : [{Area : ...}]}]
+	/*describe("#descendents()", function () {
+		it("should return at least one path down the tree", function (done) {
 
-	// returns the collections within the particular area node
+			var path = [
+				"BSB Student Services",
+				"Student Support Contact List",
+				"Student Support Services"
+			];
+
+			directory.root_area().then(function (area) {
+				area.descendents()
+			}).then(function (descendents) {
+				path.every(function (area_name) {
+					Object.keys(descendents).some(function (child)) {
+						if (child.area_name == area_name) {
+							descendents = 
+							return 
+						} else return false;
+					}
+				});
+			});
+		});
+	});*/
+
+	// returns the relevant nodes from underneath the Area
+	// -> [Area]
+	describe("#search()", function () {
+
+		it("should return at least one relevant result", function (done) {
+			directory.root_area()
+				.then(function (area) {
+					return area.search("financial ai");
+				})
+				.then(function (results) {
+					expect(results.map(function (result) {
+						return result.get_name()
+					})).to.contain("Financial Aid");
+					done();
+				})
+			;
+		});
+	})
+
+	// returns the INITIAL collections within the particular area node
 	// -> [Collection]
 	describe("#collections()", function() {
-		it("should return at least one appropriate collection");
-	});
+		it.skip("should return at least one appropriate collection",
+			function (done) {
+				var path = [
+					"BSB Student Services",
+					"Student Support Contact List",
+					"Student Support Services",
+					"Financial Aid"
+				];
 
-	// returns the relevant trees underneath the Area
-	// -> {Area : {Area : ...}}
-	describe("#filter()", function () {
-		it("should return at least one relevant result");
-		it("should preserve the correct tree layout");
-	})
+				directory.root_area().then(function (area) {
+					return area.descend_along_path(path);
+				}).then(function (area) {
+					return area.collections()
+				}).then(function (collections) {
+					expect(collections.some(function (coll) {
+						// probably should do collections first
+						return false;
+					}))
+					done();
+				});
+			}
+		);
+	});
 });
 
 // {contacts : [...], followed_by : [Collection]}
